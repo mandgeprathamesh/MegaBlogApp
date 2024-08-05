3a11f796c2bfd62811020f18d3eb5f5d4807d5a0
Prathamesh Mandge <mandge.prathamesh10@gail.com>
2024-08-05 23:46:56 +0530

    redux store added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice"
+import Header from './components/header/header/header';
+import Footer from './components/header/footer/footer';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">A blog app with appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, []);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        {/* TODO:  <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
